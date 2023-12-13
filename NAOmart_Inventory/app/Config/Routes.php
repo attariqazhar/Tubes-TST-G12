a5c928c699c489e8cd0c0ee451792b2f77a5a249
attariqazhar <18221043@mahasiswa.itb.ac.id>
2023-12-13 21:37:44 +0700

    added login & logout features

--- NAOmart_Inventory/app/Config/Routes.php
+++ NAOmart_Inventory/app/Config/Routes.php
@@ -5,5 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Pages::index');
-$routes->get('/profile', 'Pages::profile');
+$routes->get('/', 'ItemController::index');
+$routes->get('/profile', 'AccountController::index');
+$routes->get('/login', 'AccountController::login');
+$routes->get('/logout', 'AccountController::logout');
+$routes->post('/login_action', 'AccountController::login_action');
+$routes->post('/logout', 'AccountController::logout');
---
+++ NAOmart_Inventory/app/Views/layout/loginHeader.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <?php helper('html') ?>
+</head>
+<body class="flex bg-[#123D6A] font-dmsans">
+    
